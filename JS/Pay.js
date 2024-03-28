bd98fc39b123bbedc40269feb49aaa17134a824b
SamSanight <148284405+SamSanight@users.noreply.github.com>
2024-03-28 17:29:34 +0000

    small improvement and commenting

--- JS/Pay.js
+++ JS/Pay.js
@@ -1,25 +1,27 @@
-window.onload = () =>{
-    const ValidateCardNumber = (cardNumber) =>{
-        let isValid = true;
-        if(cardNumber === null || cardNumber === "" || typeof cardNumber != "string"){
+window.onload = () =>{ // make sure the page is loaded before executing js to prevent errors with getelementbyIDs
+    const ValidateCardNumber = (cardNumber) =>{ // for validating if card number is correct
+        let isValid = true; // the main boolean value for my validation
+        if(cardNumber === null || cardNumber === "" || typeof cardNumber != "string"){ // used throughout to check if value to validate is null,empty or not a string
             isValid = false;
         }
-        let re = /^(5[1-5][0-9]{14})$/;
-        if(re.test(cardNumber) === false){
+        let re = /^(5[1-5][0-9]{14})$/; // regular expresion for card Number to see if it starts with number 5, then has either number 1-5 after that and then any 14 numbers after that totaling 16 digits.
+        if(re.test(cardNumber) === false){ // tests the RE against cardNumber
             isValid = false;
         }
-        return isValid;
+        return isValid; // returns the validation function
     }
-    const ValidateMonth = (Month) =>{
+    const ValidateDate = (Month,Year) =>{
         let isValid = true;
-        if(Month === null || Month === "" || typeof Month != "string"){
+        const date = new Date(); // get the current Date and put in const date
+        const currentMonth = date.getMonth() + 1; // use date to give const currentMonth its date value and adds 1 ass getMonth starts at 0
+        const currentYear = date.getFullYear().toString().slice(-2); // uses date again to get the currentYear and makes it a string and only gets the last 2 digits to match with the format of the Year variable
+        if(Month === null || Month === "" || typeof Month != "string" || Year === null || Year === "" || typeof Year != "string"){ 
             isValid = false;
         }
-        return isValid;
-    }
-    const ValidateYear = (Year) =>{
-        let isValid = true;
-        if(Year === null || Year === "" || typeof Year != "string"){
+        if(Year < currentYear){ // check if year is before current year 
+            isValid = false;
+        } 
+        if(Year === currentYear && Month < currentMonth){ // check if the year is current and month is less than current, if not then year must be more than current so true
             isValid = false;
         }
         return isValid;
@@ -29,80 +31,102 @@ window.onload = () =>{
         if(code === null || code === "" || typeof code != "string"){
             isValid = false;
         }
-        let re = /^[0-9]{3,4}$/;
+        let re = /^[0-9]{3,4}$/; // this RE checks the security code to see if its any number between 0 and 9 and can be either 3 or 4 digits
         if(re.test(code) === false){
             isValid = false;
         }
         return isValid;
     }
-    const paymentForm = document.getElementById("PaymentForm");
-    if (paymentForm) {
-        document.getElementById("PaymentForm").addEventListener("submit", (e) =>{
-            e.preventDefault();
+    const paymentForm = document.getElementById("PaymentForm"); // gets the form by id and puts in a const var
+    if (paymentForm) { //checks if the form is not null,undefined or false
+        document.getElementById("PaymentForm").addEventListener("submit", (e) =>{ // checks if the form has been submited with the button click using an event listener
+            e.preventDefault(); // prevents the default submission of the form so that I can validate 
             let cardNumber = document.getElementById("cardNumberInput").value;
             let month = document.getElementById("month").value;
-            let year = document.getElementById("year").value;
+            let year = document.getElementById("year").value;                // creates variables from the form inputs
             let CVV = document.getElementById("CVVInput").value;
-            let lastFourDigits = cardNumber.substr(-4);
+            let lastFourDigits = cardNumber.substr(-4); // gets last four digits of the card number to use in the success page
 
-            sessionStorage.setItem('lastFourDigits', lastFourDigits);
+            sessionStorage.setItem('lastFourDigits', lastFourDigits); // saves the last four digits in session storage for success page later
 
             console.log(ValidateCardNumber(cardNumber));
-            console.log(ValidateMonth(month));
-            console.log(ValidateYear(year));
+            console.log(ValidateDate(month,year));          // testing elements to output to console
             console.log(ValidateCVV(CVV));
 
-            if(!ValidateCardNumber(cardNumber) || !ValidateMonth(month) || !ValidateYear(year) || !ValidateCVV(CVV)){
-                let errorMsg = "Invalid Input Check: "
+            if(!ValidateCardNumber(cardNumber) || !ValidateDate(month,year) || !ValidateCVV(CVV)){ // checks if any of the validation checks came through false
+                let errorMsg = "Invalid Input Check: " // creates an errorMsg and below is for the building of that message
                 if(!ValidateCVV(CVV)){
                     errorMsg += "Security Code, ";
                 }
                 if(!ValidateCardNumber(cardNumber)){
                     errorMsg += "Card Number, ";
                 }
-                if(!ValidateYear(year)){
-                    errorMsg += "Expiry Year, ";
+                if(!ValidateDate(month,year)){
+                    errorMsg += "Expiry Date, ";
                 }
-                if(!ValidateYear(month)){
-                    errorMsg += "Expiry Month, ";
-                }
-                errorMsg = errorMsg.substr(0, errorMsg.length - 2);
-                errorMsg += "!";
-                alert(errorMsg);
-                return;
-            } else{
-                let data ={
+                errorMsg = errorMsg.substr(0, errorMsg.length - 2); // subtracts the last 2 parts of the message which would have been a space and comma 
+                errorMsg += "!"; 
+                errorMsg += "\nRe-fill in the form"; // new line message on the alert
+                alert(errorMsg); // outputs the errorMsg
+                return paymentForm.reset(); // resets the form after error message output
+            } else{ // if no errors then continues to POST requests
+                let data ={ // creates a json of the data 
                     "master_card": cardNumber,
                     "exp_year": year,
                     "exp_month": month,
                     "cvv_code": CVV
                 }
-                let jsonData = JSON.stringify(data);
-                let url = "https://mudfoot.doc.stu.mmu.ac.uk/node/api/creditcard";
-
+                let jsonData = JSON.stringify(data); // puts data into a string
+                let url = "https://mudfoot.doc.stu.mmu.ac.uk/node/api/creditcard"; // url of the destination to send the data
+                
                 fetch(url, {
-                method: 'POST',
+                method: 'POST', // creates the POST request 
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: jsonData
+                body: jsonData // uses the now stringified data as the body of the POST request
                 })
-                .then(response => response.json())
+                .then(response => { // responses back from the server 
+                    const statusCode = response.status; // gets the response code,  checks each response and gives an appropriate alert to the user 
+                    if (statusCode >= 100 && statusCode < 200) { //Informational response (1xx)
+                        alert('Server Msg: ' + statusCode);
+                    } else if (statusCode >= 200 && statusCode < 300) { // Success response (2xx)
+                        window.location.href = "success.html"; // redirects to the success page
+                    } else if (statusCode >= 300 && statusCode < 400) { // Redirection response (3xx)
+                        alert('Server Msg: ' + statusCode);                       
+                        return paymentForm.reset();
+                    } else if (statusCode >= 400 && statusCode < 500) { // Client error response (4xx)
+                        alert('Server Msg: ' + statusCode);                        
+                        return paymentForm.reset();
+                    } else if (statusCode >= 500 && statusCode < 600) { // Server error response (5xx)
+                        alert('Server Msg: ' + statusCode);
+                        return paymentForm.reset();
+                    } else { //Unknown response category
+                        alert('Server Msg: ' + statusCode);
+                        return paymentForm.reset();
+                    }
+                    return response.json();
+                })
                 .then(data => {
+                    if(data.message == 'Thank you for your payment'){ // also checks response if successful
+                        window.location.href = "success.html"; // redirects to the success page
+                    } else{
+                        let errorMsg = "Error server side, please Re-fill in the form"
+                        return paymentForm.reset();
+                    }
                 console.log('Success:', data);
                 })
-                .catch((error) => {
+                .catch((error) => { // catches any errors not already caught
                 console.error('Error:', error);
                 });
             }
-            window.location.href = "success.html";
         })
     }
-    if (window.location.href.includes("success.html")) {
-        const lastFourDigits = sessionStorage.getItem('lastFourDigits');
-        if (lastFourDigits) {
-            document.getElementById("additionalText").innerHTML = "Your credit/debit card number ends in **** **** **** " + lastFourDigits;
-            sessionStorage.removeItem('lastFourDigits'); // Clear the stored value from sessionStorage
+    if (window.location.href.includes("success.html")) { // once the success redirect is trigered this code runs -----------
+        const lastFourDigits = sessionStorage.getItem('lastFourDigits'); // retrieves the last four digits that were saved to the session storage
+        if (lastFourDigits) { // if those digits are not null,empty or false or 0
+            document.getElementById("additionalText").innerHTML = "Your credit/debit card number ends in **** **** **** " + lastFourDigits; // alters the string on the success html for the card message
+            sessionStorage.removeItem('lastFourDigits'); // removes the stored value from sessionStorage 
         }
     }
 }
